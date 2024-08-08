7f4dd52531949d7e171245f6fec3ddc54b74ac39
Devendra Singh <devendrasingh@Devendras-MacBook-Air.local>
2024-08-08 13:40:59 +0530

    First Commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Footer.css'
+
+import logo from '../Assets/logo.png'
+import instagram_icon from '../Assets/instagram_icon.png'
+import pintrest_icon from '../Assets/pintester_icon.png'
+import whatsapp_icon from '../Assets/whatsapp_icon.png'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <div className="logo-image">
+        <img src={logo} alt="" />
+      </div>
+      <ul className="footer-links">
+        <li>Company</li>
+        <li>Products</li>
+        <li>Offices</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+      <div className="footer-social-icons">
+        <div className="footer-icons-container">
+            <img src={instagram_icon} alt="" />
+        </div>
+        <div className="footer-icons-container">
+            <img src={pintrest_icon} alt="" />
+        </div>
+        <div className="footer-icons-container">
+            <img src={whatsapp_icon} alt="" />
+        </div>
+      </div>
+      <div className="footer-copyright">
+        <hr />
+        <p>Made with 💚 by devendra</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
